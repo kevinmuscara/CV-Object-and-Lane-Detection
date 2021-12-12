e1bb4c939f89c862660ce334de7f4dbdf2d5e411
Kevin Muscara <nutkey5@gmail.com>
2021-12-12 17:06:16 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # CV Object and Lane Detection
- Hands on tutorial showing how to run Tensorflow Lite object detection models with hough lane detection
+ Hands on tutorial showing how to run Tensorflow Lite object detection models with hough lane detection.
+
+## Step 1
+First, let's make sure the camera interface is enabled in the Raspberry Pi Configuration Menu. Click the Pi icon in the top left corner of the screen, select Preferences -> Raspberry Pi Configuration, and go to the Interfaces tab and verify Camera is set to Enabled. If it isn't, enable it now, and reboot the Raspberry Pi.
+
+## Step 2
+Next, Open a terminal window and clone this GitHub repository by issuing the following command.
+```
+git clone https://github.com/kevinmuscara/CV-Object-and-Lane-Detection.git
+```
+
+This downloads all the required files and source code needed for this project into a folder called `CV-Object-and-Lane-Detection`, To rename it, issue the following commands.
+
+```
+mv CV-Object-and-Lane-Detection cv
+cd cv
+```
+
+## Step 3
+We will work in this directory for the remaining portions of this guide. Next, we have to create a virtual environment. 
+A virtual environment is used to prevent any conflicts between packages or libraries that are already installed on your device. 
+
+Install `virtualenv` by issuing:
+
+```
+sudo pip3 install virtualenv
+```
+
+Then, create your virtual environment by issuing:
+
+```
+python3 -m venv cv1-env
+```
+
+This will create a folder called `cv1-env` inside the project directory. This folder will hold all packages and libraries for this environment.
+Next, we'll need to activate the virtual environment by issuing:
+
+```
+source cv1-env/bin/activate
+```
+
+Anytime you close or open the terminal window, you'll need to reissue the activation command from inside the `/home/pi/cv` directory to reactivate the environment.
+
+## Step 4
+
+Next, we have to install TensorFlow, OpenCV, and all dependencies needed for both packages. To install the needed libraries, issue the following:
+
+```
+bash install.sh
+```
+
+Once all the libraries and packages finish installing, you're ready to go! 
+
+## Object Detection on webcam feed.
+
+Run the following command to start the webcam feed.
+```
+python3 webcam.py
+```
+
+If you want to mess around with the threshold parameters that determine what objects get detected for a certain percentage, run the following:
+```
+python3 webcam.py --threshold=0.5
+```
+`0.5` is the same as 50%, meaning objects will only be detected if the program is 50% sure of what the object is.
+
+To quit the program at anytime, press the `q` key.
+
+## Object Detection and lane detection with dashcam footage.
+
+AI is very rapidly taking over, one of the most well known AI projects are self driving cars. There's a lot that goes into computer vision for a self driving car, but this is a very simplistic example of a self driving cars computer vision.
+
+To run the program issue the following:
+
+```
+python3 car.py
+```
+
+Just like with the webcam object detection, if you want to mess around with the threshold parameters, issue the following:
+
+```
+python3 car.py --threshold=0.5
+```
+
+To quit the program at anytime, press the `q` key.
---
+++ car.py
@@ -0,0 +1,176 @@
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import importlib.util
+
+def do_canny(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+def do_segment(frame):
+    height = frame.shape[0]
+    polygons = np.array([[(0, height),(800,height),(300,150)]])
+    mask = np.zeros_like(frame)
+    cv2.fillPoly(mask, polygons, 255)
+    segment = cv2.bitwise_and(frame, mask)
+    return segment
+
+def calculate_lines(frame, lines):
+    left = []
+    right = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        y_intercept = parameters[1]
+        if slope < 0:
+            left.append((slope, y_intercept))
+        else:
+            right.append((slope, y_intercept))
+    
+    left_avg = np.average(left, axis=0)
+    left_line = calculate_coordinates(frame, left_avg)
+    right_avg = np.average(right, axis=0)
+    right_line = calculate_coordinates(frame, right_avg)
+    return np.array([left_line, right_line])
+
+def calculate_coordinates(frame, parameters):
+    try:
+        slope, intercept = parameters
+    except TypeError:
+        slope, intercept = 0,0
+
+    y1 = frame.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+
+def visualize_lines(frame, lines):
+    lines_visualize = np.zeros_like(frame)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(lines_visualize, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    return lines_visualize
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir', help='Folder the .tflite file is located in', default='model')
+parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite', default='detect.tflite')
+parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt', default='labelmap.txt')
+parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects', default=0.40)
+parser.add_argument('--video', help='Name of the video file', default='test.mp4')
+parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection', action='store_true')
+
+args = parser.parse_args()
+
+MODEL_NAME = args.modeldir
+GRAPH_NAME = args.graph
+LABELMAP_NAME = args.labels
+VIDEO_NAME = args.video
+min_conf_threshold = float(args.threshold)
+use_TPU = args.edgetpu
+
+pkg = importlib.util.find_spec('tflite_runtime')
+if pkg:
+    from tflite_runtime.interpreter import Interpreter
+    if use_TPU:
+        from tflite_runtime.interpreter import load_delegate
+else:
+    from tensorflow.lite.python.interpreter import Interpreter
+    if use_TPU:
+        from tensorflow.lite.python.interpreter import load_delegate
+
+if use_TPU:
+    if (GRAPH_NAME == 'detect.tflite'):
+        GRAPH_NAME = 'edgetpu.tflite'   
+
+CWD_PATH = os.getcwd()
+VIDEO_PATH = os.path.join(CWD_PATH,VIDEO_NAME)
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+if labels[0] == '???':
+    del(labels[0])
+
+if use_TPU:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT, experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+    print(PATH_TO_CKPT)
+else:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
+
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+video = cv2.VideoCapture(VIDEO_PATH)
+
+imW = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+imH = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+while(video.isOpened()):
+    ret, frame = video.read()
+    if not ret:
+      print('Reached the end of the video!')
+      break
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    canny = do_canny(frame)
+    segment = do_segment(canny)
+
+    hough = cv2.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
+    lines = calculate_lines(frame, hough)
+    lines_visualize = visualize_lines(frame, lines)
+
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
+
+    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.invoke()
+
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0] 
+    classes = interpreter.get_tensor(output_details[1]['index'])[0] 
+    scores = interpreter.get_tensor(output_details[2]['index'])[0] 
+
+    cv2.imshow('Original', frame)
+
+    for i in range(len(scores)):
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 4)
+
+            object_name = labels[int(classes[i])]
+            label = '%s: %d%%' % (object_name, int(scores[i]*100))
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+            label_ymin = max(ymin, labelSize[1] + 10) 
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) 
+
+    output = cv2.addWeighted(frame, 0.9, lines_visualize, 1, 1)
+    cv2.imshow('Computer Vision', output)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
